e6e2342654ae36be430e0b0359a3d680ccfc0426
Glen Lloyd-MacGilp <2394934l@student.gla.ac.uk>
2020-03-20 18:16:36 +0000

    added to contact-us and book. altered urls too

--- ptfinder/ptfinderApp/urls.py
+++ ptfinder/ptfinderApp/urls.py
@@ -5,4 +5,7 @@ app_name = 'ptfinder'
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('contactus/', views.contactus, name="contactus"),
+    path('book/', views.book, name="book"),
+    
 ]
--- ptfinder/ptfinderApp/views.py
+++ ptfinder/ptfinderApp/views.py
@@ -5,3 +5,14 @@ def index(request):
     context_dict = {}
 
     return render(request, 'ptfinderApp/index.html', context=context_dict)
+
+def contactus(request):
+    context_dict = {}
+    
+    return render(request, 'ptfinderApp/contact-us.html', context=context_dict)
+    
+def book(request):
+    context_dict = {}
+    
+    return render(request, 'ptfinderApp/book.html', context=context_dict)
+    
